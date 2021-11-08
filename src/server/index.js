b450466538a4ef9c4f2a33ac73ff1ac767587d0f
zlobnikov <daffodil@list.ru>
2021-11-08 18:58:10 +0300

    feat: add env vars

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const PORT = 3000;
@@ -8,4 +10,5 @@ app.get('/ping', (req, res) => res.json({ ping: 'pong' }));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+  console.log(`TOKEN: ${process.env.SECOND_TOKEN}`);
 });
