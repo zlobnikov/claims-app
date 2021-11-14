771bedcce6a80bbc903864c15e85c33d0d35b06b
zlobnikov <daffodil@list.ru>
2021-11-14 14:52:01 +0300

    feat: connect db

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 
+const { MongoClient } = require('mongodb');
 const express = require('express');
 
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_CLUSTER = process.env.DB_CLUSTER;
+const DB_NAME = process.env.DB_NAME;
+
 const PORT = 3000;
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.1thyt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) throw err;
+  // const collection = client.db(DB_NAME).collection('sample_airbnb.listingsAndReviews');
+  client.close();
+});
 
 const app = express();
 
@@ -10,5 +25,4 @@ app.get('/ping', (req, res) => res.json({ ping: 'pong' }));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
-  console.log(`TOKEN: ${process.env.SECOND_TOKEN}`);
 });
