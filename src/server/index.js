005979036b579f8802cafd2d06bf4ea85760d530
zlobnikov <daffodil@list.ru>
2021-11-15 20:11:21 +0300

    debug: uncomment db code

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,26 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const express = require('express');
 const path = require('path');
 
-// const DB_USER = process.env.DB_USER;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const DB_CLUSTER = process.env.DB_CLUSTER;
-// const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_CLUSTER = process.env.DB_CLUSTER;
+const DB_NAME = process.env.DB_NAME;
 
 const PORT = process.env.PORT || 3000;
 
 const indexHtml = path.join(__dirname, '../client/index.html');
 
-// const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.1thyt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.1thyt.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// client.connect(err => {
-//   if (err) throw err;
-//   // const collection = client.db(DB_NAME).collection('sample_airbnb.listingsAndReviews');
-//   client.close();
-// });
+client.connect(err => {
+  if (err) throw err;
+  // const collection = client.db(DB_NAME).collection('sample_airbnb.listingsAndReviews');
+  client.close();
+});
 
 const app = express();
 app.use(express.static(indexHtml));
