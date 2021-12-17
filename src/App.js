fbcdc9fa066dfca07c592f401ad2b12df2e284c0
zlobnikov <daffodil@list.ru>
2021-12-17 20:06:35 +0300

    feat: add login page component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+// import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './App.css';
+import LoginPage from './components/LoginPage';
 
-function MainPage() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Main Page</p>
-        <a className="App-link" href="/claims">to Claims Page</a>
-      </header>
-    </div>
-  );
-}
+// function MainPage() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>Main Page</p>
+//         <a className="App-link" href="/claims">to Claims Page</a>
+//       </header>
+//     </div>
+//   );
+// }
 
-function ClaimsPage() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Claims Page</p>
-        <a className="App-link" href="/">to Main Page</a>
-      </header>
-    </div>
-  );
-}
+// function ClaimsPage() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>Claims Page</p>
+//         <a className="App-link" href="/">to Main Page</a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/claims" element={<ClaimsPage />} />
-        <Route path="*" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<MainPage />} />
+    //     <Route path="/claims" element={<ClaimsPage />} />
+    //     <Route path="*" element={<MainPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <LoginPage />
   );
 }
 
---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Login Page</p>
+      </header>
+    </div>
+  );
+}
+
+export default LoginPage;
